76b43990d6647a0409ea91823ad3b78ecf6314c7
Losiek <losiek86@gmail.com>
2021-12-19 19:25:02 +0000

    initial commit

---
+++ js/app.js
@@ -0,0 +1,283 @@
+let inftech = [{
+    title: "Software Developer",
+    info: {
+        place: "Dublin",
+        pay: "€ 55k per Year",
+        added: "12.12.2021",
+        endDate: "04.01.2022",
+        locationOfWork: "Dublin City Centre",
+        experience: "2 years minimum",
+        requirements: "Java, Python, Kubernetes, Docker, AWS",
+        review: "Due to ongoing expansion, Perm Recruitment is seeking an experienced Software Developer to join our team in Dublin, Ireland. We are looking for a highly motivated engineer to become part of an agile team working on a greenfield cloud native project. This is an excellent opportunity to work with leading edge technologies in an innovative and stable company, driving the development of cutting-edge software solutions for the global mobile telecommunications market.",
+        link: "contact.html",
+        image2: "./images/software.jpg",
+    },
+},
+{
+    title: "Java Developer",
+    info: {
+        place: "Cork",
+        pay: "€ 45k per Year",
+        added: "12.12.2021",
+        endDate: "14.01.2022",
+        locationOfWork: "Cork City Centre",
+        experience: "1 year minimum",
+        requirements: "Java, Kubernetes, Docker, AWS",
+        review: "Due to ongoing expansion, Perm Recruitment is seeking an experienced Java Developer to join our team in Cork, Ireland. We are looking for a highly motivated engineer to become part of an agile team working on a greenfield cloud native project. This is an excellent opportunity to work with leading edge technologies in an innovative and stable company, driving the development of cutting-edge software solutions for the global mobile telecommunications market.",
+        link: "contact.html",
+        image2: "./images/java.jpg",
+    },
+},
+{
+    title: "Front End Developer",
+    info: {
+        place: "Dublin",
+        pay: "€ 55k per Year",
+        added: "12.12.2021",
+        endDate: "10.01.2022",
+        locationOfWork: "Dublin City Centre",
+        experience: "5 years minimum",
+        requirements: "HTML, CSS, JavaScript, React",
+        review: "Due to ongoing expansion, Perm Recruitment is seeking an experienced Front End Developer to join our team in Dublin, Ireland. We are looking for a highly motivated engineer to become part of an agile team working on a greenfield cloud native project. This is an excellent opportunity to work with leading edge technologies in an innovative and stable company, driving the development of cutting-edge software solutions for the global mobile telecommunications market.",
+        link: "contact.html",
+        image2: "./images/frontend.jpg",
+    },
+},
+];
+let finance = [{
+    title: "Trainee Accountant",
+    info: {
+        place: "Dublin",
+        pay: "€ 35k per Year",
+        added: "12.12.2021",
+        endDate: "04.01.2022",
+        locationOfWork: "Dublin City Centre",
+        experience: "1 year minimum",
+        requirements: "Proficient with excel , experience with Relate Accounts Production, Kubernetes, Logical thinker, strong numeracy skills",
+        review: "We are recruiting for a Trainee Accountant to join our accounting operations team to assist with client audits. This is an opportunity to join a fast paced, growing business that offers clear career progression opportunities. Our clients include US technology businesses, pharmaceutical companies and a range of other corporates that are expanding internationally.",
+        link: "contact.html",
+        image2: "./images/accountant.jpg",
+    },
+},
+{
+    title: "Analyst Structured Finance",
+    info: {
+        place: "Cork",
+        pay: "€ 65k per Year",
+        added: "12.12.2021",
+        endDate: "14.01.2022",
+        locationOfWork: "Cork City Centre",
+        experience: "3 year minimum",
+        requirements: "dealing with loan documentation, Strong financial analysis skills, Proficiency in the use of MS Office",
+        review: "The Analyst Structured Finance will review and interpret legal documentation to ensure accurate implementation of financial covenants, trigger events, priority of payments (waterfall) to determine cash distributions for all loans assigned, in compliance with various legal documents including but not limited to the following: Loan Agreement, Inter-creditor Deed, Cash Management Agreement and Servicing Agreement.",
+        link: "contact.html",
+        image2: "./images/analyst.jpg",
+    },
+},
+{
+    title: "Finance Analyst",
+    info: {
+        place: "Dublin",
+        pay: "€ 75k per Year",
+        added: "12.12.2021",
+        endDate: "10.01.2022",
+        locationOfWork: "Dublin City Centre",
+        experience: "5 years minimum",
+        requirements: "Qualified accountant, Highly adaptable individual, Strong Microsoft office knowledge, Good analytical skills",
+        review: "We are looking for a Qualified Accountant with an interest in learning Workday Financials. Reporting to the Finance Systems Manager (FSM), you will help rollout Workday Financials to the group. As a key member of the Finance Systems team, You will be responsible for administering, designing, implementing, advising, configuring, and monitoring Workday Financials. This includes but is not limited to configuration, reporting and integrations.",
+        link: "contact.html",
+        image2: "./images/analyst2.jpg",
+    },
+}
+];
+let healthcare = [{
+    title: "Oncologist",
+    info: {
+        place: "Dublin",
+        pay: "€ 155k per Year",
+        added: "12.12.2021",
+        endDate: "04.01.2022",
+        locationOfWork: "Dublin City Centre",
+        experience: "5 years minimum",
+        requirements: "Full Irish Medical Council Registration, MBBS",
+        review: "We are recruiting Oncology for junior doctors to take up very flexible fixed term contracts which can be for 24 months starting in January 2022. This is a great opportunity to join an expanding clinical area, which due to its structure, focuses completely on patient care with the resources to fulfil. The departments promote a strong team culture, holistic approach to nursing and foremost excellent patient care.",
+        link: "contact.html",
+        image2: "./images/onco.jpg",
+    },
+},
+{
+    title: "Staff Nurse",
+    info: {
+        place: "Dublin",
+        pay: "€ 45k per Year",
+        added: "12.12.2021",
+        endDate: "14.01.2022",
+        locationOfWork: "ublin City Centre",
+        experience: "1 year minimum",
+        requirements: "Experience within a Healthcare setting, Strong communications and Excellent attention to detail, Empathic and warm natured, 3 years experience",
+        review: "We are looking for Staff Nurses to work within an exciting healthcare project in the Dublin 2 area. Are you an experienced Staff Nurse, looking for a two day week, working 9am - 5pm? We are looking for a Staff Nurse to manage a Covid-19 On-Site Swabbing Service. The ideal candidate will enjoy working within a corporate setting, be engaging and customer focused.",
+        link: "contact.html",
+        image2: "./images/nurse.jpg",
+    },
+},
+{
+    title: "Clinical Doctor",
+    info: {
+        place: "Dublin",
+        pay: "€ 155k per Year",
+        added: "12.12.2021",
+        endDate: "10.01.2022",
+        locationOfWork: "Dublin City Centre",
+        experience: "5 years minimum",
+        requirements: "IMC registered and GMC registered, Completion of General Practice training, Over 2 years experience , Experience in digital healthcare/telemedicine",
+        review: "Our client, a leading European Digital Healthcare provider are currently recruiting for a Clinical Doctor to joint their team. As Clinical Doctor you will complete consultations for our Irish and UK patients therefore you must have IMC and GMC registration to apply for this role. Our client’s office is based in Dublin city centre but this role can be carried out largely from home.",
+        link: "contact.html",
+        image2: "./images/doctor.jpg",
+    },
+}
+];
+let manufacturing = [{
+    title: "Contract Manufacturing Specialist",
+    info: {
+        place: "Dublin",
+        pay: "€ 55k per Year",
+        added: "12.12.2021",
+        endDate: "04.01.2022",
+        locationOfWork: "Dublin City Centre",
+        experience: "2 years minimum",
+        requirements: "GMP, Python, Strong understanding of Pharmaceutical industry, Work with CMO, Support Investigation closure",
+        review: "Our client are leading Pharmaceutical and Medical Devices company that are looking for a Contract Manufacturing Specialist to join their team on an initial 12 month contract - which has a high probability of extension. In this role you will be the main point of contact for the Contract Manufacturing Partners. Below are are the responsibilities",
+        link: "contact.html",
+        image2: "./images/manufac.jpg",
+    },
+},
+{
+    title: "Senior Manufacturing Engineer",
+    info: {
+        place: "Limerick",
+        pay: "€ 45k per Year",
+        added: "12.12.2021",
+        endDate: "14.01.2022",
+        locationOfWork: "Limerick City Centre",
+        experience: "1 year minimum",
+        requirements: "Bachelors in Manufacturing, Crafting robust, Demonstrate intellectual curiosity",
+        review: "We are looking for a Senior Manufacturing Engineer to join a fast-paced, innovative, and highly visible team building our manufacturing capability. The position is based at their Limerick plant in Ireland.",
+        link: "contact.html",
+        image2: "./images/manufac1.jpg",
+    },
+},
+{
+    title: "Manufacturing Technologist",
+    info: {
+        place: "Dublin",
+        pay: "€ 55k per Year",
+        added: "12.12.2021",
+        endDate: "10.01.2022",
+        locationOfWork: "Dublin City Centre",
+        experience: "5 years minimum",
+        requirements: "GMP , Permit to work Systems, Incident/Accident Investigation procedures",
+        review: "The Manufacturing Technologist performs daily Manufacturing operations, as outlined by the Shift Lead and Area Owner, with a view to meeting the overall objectives of productivity, timeliness and cost while following GMP and EHS expectations.",
+        link: "contact.html",
+        image2: "./images/manufac2.jpg",
+    },
+},
+];
+
+let displayChoice = document.getElementById("show-me");
+let value
+let h1 = document.getElementsByClassName("job-title");
+let h2 = document.getElementsByClassName("city-title");
+let pay = document.querySelectorAll(".pay");
+let added = document.querySelectorAll(".added");
+let endDate = document.querySelectorAll(".endDate");
+let locationOfWork = document.querySelectorAll(".location");
+let experience = document.querySelectorAll(".experience");
+let requirements = document.querySelectorAll(".requirements");
+let review = document.querySelectorAll(".review");
+let link = document.querySelectorAll(".apply-link");
+let image2 = document.querySelectorAll(".image2");
+let starIcon = document.querySelectorAll(".first");
+let starIcon1 = document.querySelectorAll(".second");
+let starIcon2 = document.querySelectorAll(".third");
+let visibility = document.getElementById("show-content");
+let heightLine = document.querySelector(".large-margin");
+
+
+
+function showMovie() {
+    value = document.querySelector("select").value;
+    visibility.classList.remove("job-list");
+    if (value == 1) {
+        for (j = 0; j < inftech.length; j++) {
+            for (i = 0; i < inftech.length; i++) {
+                h1[i].innerHTML = inftech[i].title;
+                h2[i].innerHTML = inftech[i].info.place;
+                pay[i].innerHTML = "Pay: " + inftech[i].info.pay;
+                added[i].innerHTML = "Add date: " + inftech[i].info.added;
+                endDate[i].innerHTML = "End date: " + inftech[i].info.endDate;
+                locationOfWork[i].innerHTML = "Where: " + inftech[i].info.locationOfWork;
+                experience[i].innerHTML = "Experience: " + inftech[i].info.experience;
+                requirements[i].innerHTML = "Main knowledge: " + inftech[i].info.requirements;
+                review[i].innerHTML = inftech[i].info.review;
+                link[i].setAttribute("href", inftech[i].info.link);
+                image2[i].setAttribute("src", inftech[i].info.image2);
+            }
+        }
+    } else if (value == 2) {
+        for (j = 0; j < finance.length; j++) {
+            for (i = 0; i < finance.length; i++) {
+                h1[i].innerHTML = finance[i].title;
+                h2[i].innerHTML = finance[i].info.place;
+                pay[i].innerHTML = "Pay: " + finance[i].info.pay;
+                added[i].innerHTML = "Add date: " + finance[i].info.added;
+                endDate[i].innerHTML = "End date: " + finance[i].info.endDate;
+                locationOfWork[i].innerHTML = "Director: " + finance[i].info.locationOfWork;
+                experience[i].innerHTML = "Writers: " + finance[i].info.experience;
+                requirements[i].innerHTML = "Stars: " + finance[i].info.requirements;
+                review[i].innerHTML = finance[i].info.review;
+                link[i].setAttribute("href", finance[i].info.link);
+                image2[i].setAttribute("src", finance[i].info.image2);
+            }
+        }
+    } else if (value == 3) {
+        for (j = 0; j < healthcare.length; j++) {
+            for (i = 0; i < healthcare.length; i++) {
+                h1[i].innerHTML = healthcare[i].title;
+                h2[i].innerHTML = healthcare[i].info.place;
+                pay[i].innerHTML = "Pay: " + healthcare[i].info.pay;
+                added[i].innerHTML = "Add date: " + healthcare[i].info.added;
+                endDate[i].innerHTML = "End date: " + healthcare[i].info.endDate;
+                locationOfWork[i].innerHTML = "Director: " + healthcare[i].info.locationOfWork;
+                experience[i].innerHTML = "Writers: " + healthcare[i].info.experience;
+                requirements[i].innerHTML = "Stars: " + healthcare[i].info.requirements;
+                review[i].innerHTML = healthcare[i].info.review;
+                link[i].setAttribute("href", healthcare[i].info.link);
+                image2[i].setAttribute("src", healthcare[i].info.image2);
+            }
+        }
+    } else if (value == 4) {
+        for (j = 0; j < manufacturing.length; j++) {
+            for (i = 0; i < manufacturing.length; i++) {
+                h1[i].innerHTML = manufacturing[i].title;
+                h2[i].innerHTML = manufacturing[i].info.place;
+                pay[i].innerHTML = "Pay: " + manufacturing[i].info.pay;
+                added[i].innerHTML = "Add date: " + manufacturing[i].info.added;
+                endDate[i].innerHTML = "End date: " + manufacturing[i].info.endDate;
+                locationOfWork[i].innerHTML = "Director: " + manufacturing[i].info.locationOfWork;
+                experience[i].innerHTML = "Writers: " + manufacturing[i].info.experience;
+                requirements[i].innerHTML = "Stars: " + manufacturing[i].info.requirements;
+                review[i].innerHTML = manufacturing[i].info.review;
+                link[i].setAttribute("href", manufacturing[i].info.link);
+                image2[i].setAttribute("src", manufacturing[i].info.image2);
+            }
+        }
+    }
+}
+
+function thanks() {
+    alert("Thank You for contacting us");
+}
+
+
+
+// displayChoice.addEventListener("click", showMovie);
